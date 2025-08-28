64bb36c470a4d77f7637584a11f70001ad9be1ea
pantamisa <samuelcausil@gmail.com>
2025-08-28 10:41:37 -0500

    feat:crear-estructraproyecto

---
+++ Model/Menu.py
@@ -0,0 +1,17 @@
+
+
+
+while True:
+    print("--Veterinaria EL Zancudo--")
+    print("--------------------------")
+    print("MENU DE APLICACION")
+    print("1. manejo animales")
+    print("2. Crear cita")
+    print("3. pagar facturas")
+    
+    opcion=input("Ingrese una opcion")
+    
+    match opcion:
+        case "1":
+            print("---sub menu---")
+    
